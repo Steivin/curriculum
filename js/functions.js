cc537258df8c872c909c9d16883868a08b33f24d
Steivin <steivin2000@gmail.com>
2024-05-10 14:28:08 -0500

    JS class continuation

--- js/functions.js
+++ js/functions.js
@@ -44,6 +44,8 @@ function load_page() {
         showConfirmButton: false,
         timer: 2000
     });
+
+    document.getElementById("text").innerText = ("el array a calcular es: "+array_num);
 }
 
 function send_form() {
@@ -73,3 +75,55 @@ function send_form() {
         document.getElementById("print").innerText = "Su nombre es: " + name +" "+ last_name;
     }
 }   
+
+//  ARRAYS
+var array_semana = ["Lunes", "Martes", "Miesrcoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+var array_gral = [1,2,2.5,"abc", "Soy Steivin"];
+var array_num = [1,2,3,4,5,6,7,8,9];
+
+var array_multi= [
+    {name:"Steivin", age:23,color:"Azul"},
+    {name:"Wilder", age:32,color:"Rojo"},
+    {name:"Rosa", age:25,color:"Rosa"},
+    {name:"Camila", age:2,color:"Verde"},
+
+];
+
+var json_ejm = {
+    name: "Steivin",
+    lastname: "Cardenas",
+    phone: "3114552823",
+    email: "steivin2000@gmail.com"
+
+}
+
+console.log(array_semana);
+console.log(array_gral);
+console.log(array_num);
+console.log(array_multi);
+console.log(json_ejm);
+
+function action() {
+    // alert(array_num)
+    //swal.fire(""+array_num);
+    console.log(array_num);
+    var result = 0;
+    for(let i=0;i<array_num.length;i++){
+        result = result+array_num[i]
+    }
+    document.getElementById("result").innerHTML = "<strong>El resultado es : </strong";
+    document.getElementById("result").style.color = "green";
+    // CON UN FOR
+    var total = 0;
+    for(var j=0;j<array_multi.length;j++){
+        total = array_multi[j].age + total;
+    }
+    alert(total);
+
+    //CON UN WHILE
+    var total = 0;
+    var w = 0;
+    while(w<array_multi.length){
+        total_dos = array_multi[w].age
+    }
+}
