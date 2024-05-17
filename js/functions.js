4780611d8fff8f799eeba719e253faa4d9a84156
Steivin <steivin2000@gmail.com>
2024-05-17 12:26:26 -0500

    actualizing

--- js/functions.js
+++ js/functions.js
@@ -11,119 +11,131 @@ console.log("HOLA DESDE LA CONSOLA");
 //   icon: "error"
 //  }); 
 
-// VARIABLES Y TIPOS DE DATOS
-const pi = 3.14;  //decimal
-const pul = 2.54;
-var name = "Steivin Cardenas"; //string
-let edad = 23;  //entero
-var ver = true; //boolean
-let fal = false; //boolean
+// VARIABLES Y TIPO DE DATOS
+const pi = 3.14; // decimal
+var name = "Wilder Duarte"; // string
+let edad = 32; // entero
+let edad_string = "32"; // string
+var bool = true; // false booleanos
 
-//OPERADORES BÁSICOS
-var suma = 5 + 2;
+//OPERADORES BÁSICOS + - / * %
+var suma = 5 - 2;
 console.log(suma);
-var var_uno = 10;
-var var_dos = 5;
-console.log(var_uno - var_dos);
-console.log(var_uno * var_dos);
-console.log(var_uno / var_dos);
-console.log(var_uno % var_dos);
+console.log(edad_string + suma);
+var one = 10;
+var two = 20;
+console.log(one + two);
+console.log(one - two);
+console.log(one * two);
+console.log(one / two);
+console.log(one % two);
 
-// OPERADORES DE COMPARACIÓN 
-// = Asignaciòn
-// == comparacion (valor)
-// === estrictamente comparado (valor y el tipo de dato)
-// > / >= / <= / <>
+//OPERADORES DE COMPARACIÓN
+// < > >= <= <> 
+//= Asignación
+//== Comparación 5=="5"
+//=== Comparación 5=="5"
+//==== es una mentira del profesor
+//OPERADORES LÓGICOS
+// AND && OR ||
 
-
-console.log("Mi edad es: " + edad);
+console.log("Mi nombre " + name + " tiene como edad: " + edad);
 
 function load_page() {
     Swal.fire({
-        imageUrl: "https://ufpso.edu.co/administradoru15/ventana/banner%20Admitidos_Mesa%20de%20trabajo%201%20(3)%20(1).jpg",
-        showConfirmButton: false,
-        timer: 2000
+        imageUrl: "../img/WilderDuarte.png",
+        imageAlt: "A tall image",
+        showConfirmButton: false
     });
-
-    document.getElementById("text").innerText = ("el array a calcular es: "+array_num);
+    document.getElementById("valores").value = array_ejm;
 }
 
-function send_form() {
+function send_info() {
     let name = document.getElementById("name").value;
     let last_name = document.getElementById("last_name").value;
     if (name.length == 0 || last_name.length == 0) {
         Swal.fire({
-            title: "The Internet?",
-            text: "That thing is still around?",
+            title: "Campos Vacíos",
+            text: "Alguno de los campos se encuentra vacío",
             icon: "error"
         });
-
         if (name.length == 0) {
-            document.getElementById("name").style.border = "2p solid red"
+            document.getElementById("name").style.border = "2px solid red"
         }
-        else {
-            document.getElementById("name").style.border = "2p solid green"
+        if (last_name.length == 0) {
+            document.getElementById("last_name").style.background = "red"
         }
-        if (name.length == 0) {
-            document.getElementById("last_name").style.border = "2p solid red"
-        }
-        else {
-            document.getElementById("last_name").style.border = "2p solid green"
-        }
+    } else {
+        document.getElementById("print").innerHTML = "<strong>" + name + " " + last_name + "</strong>";
     }
-    else{
-        document.getElementById("print").innerText = "Su nombre es: " + name +" "+ last_name;
-    }
-}   
-
-//  ARRAYS
-var array_semana = ["Lunes", "Martes", "Miesrcoles", "Jueves", "Viernes", "Sabado", "Domingo"];
-var array_gral = [1,2,2.5,"abc", "Soy Steivin"];
-var array_num = [1,2,3,4,5,6,7,8,9];
-
-var array_multi= [
-    {name:"Steivin", age:23,color:"Azul"},
-    {name:"Wilder", age:32,color:"Rojo"},
-    {name:"Rosa", age:25,color:"Rosa"},
-    {name:"Camila", age:2,color:"Verde"},
-
-];
-
-var json_ejm = {
-    name: "Steivin",
-    lastname: "Cardenas",
-    phone: "3114552823",
-    email: "steivin2000@gmail.com"
-
 }
 
-console.log(array_semana);
-console.log(array_gral);
+//ARRAYS
+var dias_sem = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+var array_num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var array_mix = [1, 2.5, "abc"];
+var array_mul = [
+    { name: "Wilder", age: 32, color: "green" },
+    { name: "Anyi", age: 33, color: "white" },
+    { name: "Celeste", age: 3, color: "black" },
+    { name: "Antonella", age: 2, color: "gray" }
+]
+
+var json_ejm = {
+    name: "Wilder",
+    lastname: "Duarte",
+    phone: "3168667751",
+    email: "waduarten@ufpso.edu.co"
+}
+
+console.log(dias_sem);
 console.log(array_num);
-console.log(array_multi);
+console.log(array_mix);
+console.log(array_mul);
 console.log(json_ejm);
 
-function action() {
-    // alert(array_num)
-    //swal.fire(""+array_num);
-    console.log(array_num);
-    var result = 0;
-    for(let i=0;i<array_num.length;i++){
-        result = result+array_num[i]
-    }
-    document.getElementById("result").innerHTML = "<strong>El resultado es : </strong";
-    document.getElementById("result").style.color = "green";
-    // CON UN FOR
-    var total = 0;
-    for(var j=0;j<array_multi.length;j++){
-        total = array_multi[j].age + total;
-    }
-    alert(total);
+for (let i = 0; i < dias_sem.length; i++) {
+    console.log(dias_sem[i]);
+}
 
-    //CON UN WHILE
-    var total = 0;
-    var w = 0;
-    while(w<array_multi.length){
-        total_dos = array_multi[w].age
-    }
+var j = 0;
+while (j < array_num.length) {
+    console.log(array_num[j]);
+    j++
+}
+
+var acum = 0;
+for (let h = 0; h < array_num.length; h++) {
+    acum += array_num[h]
+    // acum = acum + array_num[h]
+}
+console.log(acum);
+
+var suma_age = 0;
+for (let a = 0; a < array_mul.length; a++) {
+    suma_age += array_mul[a].age;
+}
+console.log(suma_age);
+document.getElementById("print_age").innerText = "El valor de la suma es: " + suma_age;
+document.getElementById("print_age_eti").innerHTML = "<strong>El valor de la suma es: " + suma_age + "</strong>";
+
+function limpiar() {
+    document.getElementById("valores").value = "";
+    document.getElementById("print_age").innerText = "";
+    document.getElementById("print_age_eti").innerHTML = "";
+}
+
+var array_ejm = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function agregar() {
+    array_ejm.push(10);
+    document.getElementById("valores").value = array_ejm;
+}
+function eliminar() {
+    array_ejm.pop();
+    document.getElementById("valores").value = array_ejm;
+}
+function imp_rever() {
+    document.getElementById("text_rever").innerText = array_ejm.reverse();
+    document.getElementById("valores").disabled = false;
 }
